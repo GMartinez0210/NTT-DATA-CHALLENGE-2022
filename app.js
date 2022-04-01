c337bf3067cb2ca913affdd79ec27bd36fa3b7c1
GMartinez0210 <genaro010204@gmail.com>
2022-04-01 14:47:40 -0500

    Starting Node
    Añadí Node, express, body-parser, mongoose, passport, passport-local, passport-local-mongoose y EJS.
    
    Login listo

---
+++ app.js
@@ -0,0 +1,26 @@
+const express =  require("express")
+const bodyParser = require("body-parser")
+const ejs = require("ejs")
+const mongoose = require("mongoose")
+const passport = require("passport")
+
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static("public"))
+app.set("view engine", "ejs")
+
+app.route("/login")
+    .get(function(req, res) {
+        res.render("login")
+    })
+
+
+app.route("/")
+    .get(function(req, res) {
+        res.redirect("/login")
+    })
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server's running on port 3000")
+})
